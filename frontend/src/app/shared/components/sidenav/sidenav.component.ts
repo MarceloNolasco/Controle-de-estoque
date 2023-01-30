e6dd67d14fdda3cd1ad656cebd70d8d04e353c45
Marcelo Nolasco <omarcelonolasco@gmail.com>
2023-01-30 20:04:33 -0300

    modificating routing

--- frontend/src/app/shared/components/sidenav/sidenav.component.ts
+++ frontend/src/app/shared/components/sidenav/sidenav.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.scss']
+  styleUrls: ['./sidenav.component.scss'],
 })
 export class SidenavComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
---
+++ frontend/src/app/shared/components/sidenav/sidenav.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from 'src/app/shared/shared.module';
+
+@NgModule({
+  imports: [CommonModule, SharedModule],
+  declarations: [],
+})
+export class SidenavModule {}
