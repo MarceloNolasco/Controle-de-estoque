bb2f81ea6d2fb1597d2160336e7d7de5a09c3a1f
Marcelo Nolasco <omarcelonolasco@gmail.com>
2023-02-07 18:33:12 -0300

    new products

--- frontend/src/app/pages/login/login.module.ts
+++ frontend/src/app/pages/login/login.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
-import { SharedModule } from 'src/app/shared/shared.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from 'src/app/shared/material/material.module';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, MaterialModule],
   declarations: [LoginComponent],
   exports: [LoginComponent],
 })
